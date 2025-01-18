e626f08fccf89e9718429e4bcc3cfdbb7ef4ce46
Aridsondez jerome <aridsondez@Aridsondezs-MacBook-Pro.local>
2025-01-18 14:28:30 -0500

    creating php files

---
+++ api/db.php
@@ -0,0 +1,9 @@
+//Getting the database from the Godady website 
+//1. Get Digitial Ocean IP
+//2. Select the db that we are using 
+//3. Get the SQL password
+//4. Connect to the databse
+<?php
+
+
+?>
