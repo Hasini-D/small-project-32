1ef5816222c4349adc999cc46dca8400b42786fc
Aridsondez <schoolaridsondez@gmail.com>
2025-01-27 13:27:08 -0500

    trying to fix CORS

--- LAMPAPI/Login.php
+++ LAMPAPI/Login.php
@@ -1,9 +1,15 @@
 
 <?php
-	header("Access-Control-Allow-Origin: *"); // Allow requests from any origin
-	header("Content-Type: application/json; charset=UTF-8"); // Set the content type
-	header("Access-Control-Allow-Methods: POST"); // Allow POST requests
-	header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200); // Respond OK for preflight
+    exit();
+}
+
 
 	$inData = getRequestInfo();
 	
